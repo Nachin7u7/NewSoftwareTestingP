5cc0f37842cbb9805dcce209748f79795c39953b
Ricardo Valencia <ignacio.valencia.sauma@gmail.com>
2022-02-22 08:30:34 -0400

    tercer commit

---
+++ lib/src/test/java/SoftwareTestingExtra/BecaTest.java
@@ -0,0 +1,11 @@
+package SoftwareTestingExtra;
+
+import org.junit.*;
+import org.mockito.*;
+
+public class BecaTest {
+
+	Nota nota = Mockito.mock(Nota.class);
+	MockedStatic<Helpers> helpers = Mockito.mockStatic(Helpers.class);
+
+}
